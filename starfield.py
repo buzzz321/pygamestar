1d263e96048da5183ce7def849dfc5b2755f85a9
buzzz321 <anders.olme@gmail.com>
2016-01-02 08:50:35 +0100

    Fixed some movements, still abit buggy though

--- starfield.py
+++ starfield.py
@@ -6,20 +6,61 @@ from collections import namedtuple
 
 width = 1024
 height = 768
-Pixel = namedtuple('Pixel', ['x', 'y'], verbose=False)
+
+
+# Pixel = namedtuple('Pixel', ['x', 'y', 'xa', 'ya'], verbose=False)
+
+class Pixel:
+    def __init__(self, x, y, xa=0, ya=0):
+        self.x = x
+        self.y = y
+        self.xa = xa
+        self.ya = ya
+        self.ox = x
+        self.oy = y
+
+    def move(self, width, height):
+        self.x += self.xa
+        self.y += self.ya
+
+        if self.x <= 0 or self.x >= width:
+            self.x = random.randint(0, width - 1)
+            self.y = random.randint(0, height - 1)
+            self.star_direction(width, height)
+
+        if self.y <= 0 or self.y >= height:
+            self.x = random.randint(0, width - 1)
+            self.y = random.randint(0, height - 1)
+            self.star_direction(width, height)
+
+    def star_direction(self, width, height):
+
+        if self.x < width / 2 and self.y < height / 2:
+            self.xa = -1
+            self.ya = -1
+
+        if self.x >= width / 2 and self.y >= height / 2:
+            self.xa = 1
+            self.ya = 1
+
+        if self.x > width / 2 and self.y < height / 2:
+            self.xa = 1
+            self.ya = -1
+
+        if self.x < width / 2 and self.y > height / 2:
+            self.xa = -1
+            self.ya = 1
 
 
 def setup():
-    field = []
+    data = []
     for n in range(0, 1000):
-        field.append(Pixel(random.randint(0, width - 1),
-                            random.randint(0, height - 1)))
+        pixel = Pixel(random.randint(0, width - 1),
+                      random.randint(0, height - 1), 0, 0)
+        pixel.star_direction(height, width)
+        data.append(pixel)
 
-    return field
-
-
-def move_star(height, width, pixel):
-    pass
+    return data
 
 
 screen = pygame.display.set_mode((width, height))
@@ -27,10 +68,16 @@ screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 running = True
 starfield = setup()
+black = (0, 0, 0)
 
 while running:
+    screen.fill(black)
+    for pixel in starfield:
+        pixel.move(height, width)
 
-    for x, y in starfield:
+        x = pixel.x
+        y = pixel.y
+
         red = random.randint(0, 255)
         green = random.randint(0, 255)
         blue = random.randint(0, 255)
