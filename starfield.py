404af8955770ed4312413067b356d8a5353f22cc
buzzz321 <anders.olme@gmail.com>
2016-01-04 11:10:53 +0100

    Fixed it so it looks nice

--- starfield.py
+++ starfield.py
@@ -2,86 +2,71 @@
 
 import pygame
 import random
-from collections import namedtuple
 
-width = 1024
-height = 768
+WIDTH = 1024
+HEIGHT = 768
 
 
 # Pixel = namedtuple('Pixel', ['x', 'y', 'xa', 'ya'], verbose=False)
 
 class Pixel:
-    def __init__(self, x, y, xa=0, ya=0):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.xa = xa
-        self.ya = ya
+        self.dx = random.uniform(-1, 1)
+        self.dy = random.uniform(-1, 1)
 
     def move(self, width, height):
-        self.x += self.xa
-        self.y += self.ya
+        self.y = self.y + self.dy
+        self.x = self.x + self.dx
 
         if self.x <= 0 or self.x >= width:
-            self.x = random.randint(0, width - 1)
-            self.y = random.randint(0, height - 1)
-            self.star_direction(width, height)
+            self.x = width / 2 + random.randint(-20, 20)
+            self.y = height / 2 + random.randint(-10, 10)
 
         if self.y <= 0 or self.y >= height:
-            self.x = random.randint(0, width - 1)
-            self.y = random.randint(0, height - 1)
-            self.star_direction(width, height)
-
-    def star_direction(self, width, height):
-
-        if self.x < width / 2 and self.y < height / 2:
-            self.xa = -1
-            self.ya = -1
-
-        if self.x >= width / 2 and self.y >= height / 2:
-            self.xa = 1
-            self.ya = 1
-
-        if self.x > width / 2 and self.y < height / 2:
-            self.xa = 1
-            self.ya = -1
-
-        if self.x < width / 2 and self.y > height / 2:
-            self.xa = -1
-            self.ya = 1
+            self.x = width / 2 + random.randint(-20, 20)
+            self.y = height / 2 + random.randint(-10, 10)
 
 
 def setup():
     data = []
-    for n in range(0, 100):
-        pixel = Pixel(random.randint(0, width - 1),
-                      random.randint(0, height - 1), 0, 0)
-        pixel.star_direction(height, width)
+    for n in range(0, 1000):
+        pixel = Pixel(WIDTH / 2, HEIGHT / 2)
         data.append(pixel)
 
     return data
 
 
-screen = pygame.display.set_mode((width, height))
+pygame.init()
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+pygame.display.set_caption('starfield')
+
+print pygame.display.Info()
 
 clock = pygame.time.Clock()
 running = True
 starfield = setup()
 black = (0, 0, 0)
 
+s = pygame.Surface((20, 20))  # the size of your rect
+s.set_alpha(128)  # alpha level
+s.fill((0, 0, 0))  # this fills the entire surface
+
 while running:
     screen.fill(black)
     for pixel in starfield:
-        pixel.move(width, height)
+        pixel.move(WIDTH, HEIGHT)
 
-        x = pixel.x
-        y = pixel.y
+        x = int(round(pixel.x))
+        y = int(round(pixel.y))
 
-        red = random.randint(0, 255)
-        green = random.randint(0, 255)
-        blue = random.randint(0, 255)
+        red = 250  # random.randint(0, 255)
+        green = 250  # random.randint(0, 255)
+        blue = 250  # random.randint(0, 255)
 
         screen.set_at((x, y), (red, green, blue))
-
+        screen.blit(s, (WIDTH / 2 - 10, HEIGHT / 2 - 10))  # (0,0) are the top-left coordinates
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
